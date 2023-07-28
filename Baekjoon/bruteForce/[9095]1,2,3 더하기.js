7a22febe37a98c4d02b7c9265dd5146ffe67a6d7
DevSORA <sodaoov@gmail.com>
2023-07-28 19:20:11 +0900

    solve 9095

---
+++ Baekjoon/bruteForce/[9095]1,2,3 더하기.js
@@ -0,0 +1,23 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync("./Baekjoon/input.txt")
+  .toString()
+  .trim()
+  .split("\r\n")
+  .map(Number);
+
+input.shift();
+
+let sumArr = [];
+
+let memo = [1, 2, 4];
+
+for (let i = 2; i < 10; i++) {
+  memo.push(memo[i] + memo[i - 1] + memo[i - 2]);
+}
+
+for (let i = 0; i < input.length; i++) {
+  sumArr.push(memo[input[i] - 1]);
+}
+
+console.log(sumArr.join("\n"));
