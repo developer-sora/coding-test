0a4828fff7da9d51e62e1571e43f9fc0678157f6
DevSORA <sodaoov@gmail.com>
2023-08-11 09:33:18 +0900

    solve 15654

---
+++ Baekjoon/BackTracking/[15654]N과 M(5).js
@@ -0,0 +1,34 @@
+let fs = require("fs");
+let input = fs
+  .readFileSync("./Baekjoon/input.txt")
+  .trim()
+  .toString()
+  .split("\r\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const output = [];
+let result = "";
+
+function dfs(cnt) {
+  if (cnt === M) {
+    result += `${output.join(" ")}\n`;
+    return;
+  }
+  for (let i = 0; i < N; i++) {
+    if (output.includes(arr[i])) {
+      continue;
+    }
+    output.push(arr[i]);
+    dfs(output.length);
+    output.pop();
+  }
+}
+
+dfs(0);
+
+console.log(result.trim());
