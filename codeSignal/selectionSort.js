3a52a6e6673419ff6ccfb1316795f70ed269ffe2
developer-sora <sodaoov@gmail.com>
2025-06-14 00:02:15 +0900

    선택정렬 모범답안 추가

--- codeSignal/selectionSort.js
+++ codeSignal/selectionSort.js
@@ -18,3 +18,22 @@ function selectionSort(arr) {
 }
 
 module.exports = { selectionSort };
+
+// 모범답안
+
+function selectionSort(arr) {
+  const n = arr.length;
+  for(let i = 0; i < n - 1; i++){
+    let minIndex = i;
+    for(let j = i + 1; j < n; j++){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j;
+      }
+    }
+    [arr[i],arr[minIndex]] = [arr[minIndex], arr[i]]; 
+  }
+
+  return arr;
+}
+
+module.exports = { selectionSort };
